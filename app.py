b4351062d748ce210e956874426f9cf6dde40865
William Benton <self@willbenton.com>
2021-10-06 10:48:39 -0500

    routing complete

--- app.py
+++ app.py
@@ -9,20 +9,50 @@ import requests
 import folium
 
 class AddressForm(FlaskForm):
-    address = StringField('address', validators=[DataRequired()])
+    start_address = StringField('Start address', validators=[DataRequired()])
+    end_address = StringField('End address', validators=[DataRequired()])
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "baricades misterieuses"
 
+def resolve_address(address, endpoint="https://nominatim.openstreetmap.org"):
+    api = f"{endpoint}/search"
+
+    result = requests.get(api, params={"q": address, "format" : "jsonv2"})
+    result_struct = result.json()[0]
+    return (result_struct["lat"], result_struct["lon"])
+
+
+def resolve_duration_and_distance(start_lon, start_lat, end_lon, end_lat, endpoint="http://127.0.0.1:5000"):
+    api = f"{endpoint}/table/v1/driving/{start_lon},{start_lat};{end_lon},{end_lat}"
+
+    result = requests.get(api, params={"annotations": "duration,distance"})
+    max_duration = max([x for xs in result.json()['durations'] for x in xs])
+    max_distance = max([x for xs in result.json()['distances'] for x in xs])
+
+    return (max_duration, max_distance)
+
+def icon_for_color(color="red"):
+    return folium.map.Icon(color=color)
+
 @app.route('/map')
 @app.route('/map/')
 def index():
-    lat = float(request.args.get("lat", "43.0730556"))
-    lon = float(request.args.get("lon", "-89.4011111"))
-    marker = folium.Marker([lat, lon], popup="")
-    folium_map = folium.Map(location=(lat, lon), zoom_start=14)
+    lat1 = float(request.args.get("lat1", request.args.get("lat", "40.7769271")))
+    lon1 = float(request.args.get("lon1", request.args.get("lon", "-73.873965")))
+    
+    lat2 = float(request.args.get("lat2", "40.768082"))
+    lon2 = float(request.args.get("lon2", "-73.981893"))
+
+    duration, distance = resolve_duration_and_distance(lon1, lat1, lon2, lat2)
+
+    folium_map = folium.Map(location=(lat1, lon1), zoom_start=14)
+
+    start_marker = folium.Marker([lat1, lon1], popup="Route start", icon=icon_for_color(color="lightblue")).add_to(folium_map)
+    end_marker = folium.Marker([lat2, lon2], popup="Route end", icon=icon_for_color(color="red")).add_to(folium_map)
+    
     _map = folium_map._repr_html_()
-    return render_template('map.html', themap=_map)
+    return render_template('map.html', themap=_map, trip_distance=distance, trip_duration=duration)
 
 @app.route('/lookup', methods=["GET"])
 def querypage():
@@ -33,13 +63,12 @@ def querypage():
 def lookup():
     form = AddressForm()
     if form.validate_on_submit():
-        query = form.address.data
-        print(query)
-        result = requests.get("https://nominatim.openstreetmap.org/search", params={"q": query, "format" : "jsonv2"})
-        print(result.json())
-        result_struct = result.json()[0]
-        print(result_struct)
-        return redirect(url_for("index", lat=result_struct["lat"], lon=result_struct["lon"]))
+        start_query = form.start_address.data
+        end_query = form.end_address.data
+        start_lat, start_lon = resolve_address(start_query)
+        end_lat, end_lon = resolve_address(end_query)
+
+        return redirect(url_for("index", lat1=start_lat, lon1=start_lon, lat2=end_lat, lon2=end_lon))
 
     return "no way"
 
